5ca503a35b4d72bc89d326f8d47bd6fac5837010
hudakdidit <hudak.jonathan@gmail.com>
2014-01-14 13:04:37 -0800

    fixed share pop links

--- share.php
+++ share.php
@@ -2,8 +2,8 @@
 <a href="#" class="icon-plus shareToggle"></a>
 <div class="shareButtons">
 <a href="#" id="share-email"><span class="icon-envelope"></span></a>
-<a href="#" class="<?php if(!is_mobile){ echo 'pop'; } ?>" id="share-fb"><span class="icon-facebook"></span></a>
-<a href="#" class="<?php if(!is_mobile){ echo 'pop'; } ?>" id="share-tumblr"><span class="icon-tumblr"></span></a>
-<a href="#" class="<?php if(!is_mobile){ echo 'pop'; } ?>" id="share-twitter"><span class="icon-twitter"></span></a>
+<a href="#" class="<?php if(!is_mobile()){ echo 'pop'; } ?>" id="share-fb"><span class="icon-facebook"></span></a>
+<a href="#" class="<?php if(!is_mobile()){ echo 'pop'; } ?>" id="share-tumblr"><span class="icon-tumblr"></span></a>
+<a href="#" class="<?php if(!is_mobile()){ echo 'pop'; } ?>" id="share-twitter"><span class="icon-twitter"></span></a>
 </div>
 </section>
